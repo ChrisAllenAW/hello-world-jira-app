eecdd42466c3b793395a8e7efe3ed3e2fe0cdb1c
Christopher Allen <christopher.allen@assetworks.com>
2023-06-09 14:36:06 -0700

    Add code to log number of comments on issue

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,26 @@
-import ForgeUI, { render, Fragment, Text, IssuePanel } from '@forge/ui';
+import ForgeUI, { render, Fragment, Text, IssuePanel, useProductContext, useState } from '@forge/ui';
+import api, { route } from "@forge/api";
 
 const App = () => {
+  const context = useProductContext();
+  const [comments] = useState(async () => await fetchCommentsForIssue(context.platformContext.issueKey));
+
+  console.log(`Number of comments on this issue: ${comments.length}`);
+
   return (
     <Fragment>
       <Text>Hello world!</Text>
     </Fragment>
   );
+};
+
+const fetchCommentsForIssue = async (issueIdOrKey) => {
+  const res = await api
+    .asUser()
+    .requestJira(route`/rest/api/3/issue/${issueIdOrKey}/comment`);
+
+  const data = await res.json();
+  return data.comments;
 };
 
 export const run = render(
